5498653256716679239c2bf4c1984e24cf3a364b
Siam Talukder <73644547+siamtalukder484@users.noreply.github.com>
2023-10-03 21:53:34 +0600

    project upload

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+tailwind.config = {
+    theme: {
+      extend: {
+        colors: {
+          primary: '#FFD2A4',
+          navbg: '#0D0D0D',
+          navbtn: '#1C1D28',
+          subheading: '#161722',
+          serviceitembg: '#FFF5EB',
+        },
+        maxWidth: {
+            'container': '1280px',
+        },
+        fontFamily: {
+            robotocondenced: "Roboto Condensed, sans-serif",
+        },
+        backgroundImage: {
+          'banner': "url('images/banner.jpg')",
+        },
+        fontSize: {
+          bannerheading: '64px',
+        }
+      }
+    }
+  }
